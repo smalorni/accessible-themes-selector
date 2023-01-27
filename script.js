071fd79b15d34d8d1ffb7a28da3a95fb27c05e2a
Samantha Malorni <samantha.ann.malorni@gmail.com>
2023-01-26 20:14:20 -0500

    created default color, added hex codes, replaced inputs with buttons

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
-//create a function that when a button is clicked, it will change the color of background
+//create a function that when a button is clicked, it will change the color of background to the specific button color
+
+const background = document.querySelector('body');
+
+const defaultColor = document.body.style.background = '#ce0606';
+
 function changeBackground(color) {
-  document.body.style.backgroundColor = color;
+    background.style.backgroundColor = color;
 }
 
 window.setTimeout('changeBackground()', 1000);
